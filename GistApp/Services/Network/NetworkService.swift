6b942b373af16fa9d46adb525e90036d1cb9e4d4
hmaraegor <hmaraegor@gmail.com>
2020-07-17 17:53:08 +0300

    refactor: delete old network services

--- GistApp/Services/Network/NetworkService.swift
+++ GistApp/Services/Network/NetworkService.swift
@@ -8,39 +8,65 @@
 
 import Foundation
 
-class NetworkService {
+class NetworkService2<T: Codable> {
+    typealias Completion = (Result<T, NetworkServiceError>) -> ()
+    var decodingDataType: DataType?
     
-    private let decodeService = DecodeService()
+    func get(url: String, decodingDataType: DataType, _ completion: @escaping Completion) {
+        self.decodingDataType = decodingDataType
+        performHTTPRequest(url: url, method: "GET", data: nil, params: nil, completion)
+    }
     
-    func performHTTPRequest<T: Codable>(decodingDataType: DataType = .json, url: String, completionHandler:
-        @escaping (Result<T, NetworkServiceError>) -> ()) {
+    func post<T: Codable>(url: String, model: T, /* params: [String: Any], */completion: @escaping Completion) {
+        let (encodedData, error) = EncodeService().encodeData(model: model)
+                if error != nil { completion(.failure(.modelEncoding)) }
+                guard encodedData != nil else { return }
+        performHTTPRequest(url: url, method: "POST", data: encodedData, params: nil/* params */, completion)
+    }
     
+    func put(url: String, _ completion: @escaping Completion) {
+        performHTTPRequest(url: url, method: "PUT", data: nil, params: nil, completion)
+    }
+    
+    // MARK: - Private
+    private func performHTTPRequest(url: String, method: String, data: Data?, params: [String: Any]?, _ completion: @escaping Completion) {
         
-        guard let url = URL(string: url) else { completionHandler(.failure(.badURL))
+        guard let url = URL(string: url) else {
+            completion(.failure(.badURL))
             return
         }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method
+        urlRequest.httpBody = data
         
-        let session = URLSession.shared
-        session.dataTask(with: url) { (data, response, error) in
+        // add params
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             guard let response = response else {
-                completionHandler(.failure(.noResponse))
+                completion(.failure(.noResponse))
                 return }
             guard let data = data else {
-                completionHandler(.failure(.noData))
+                completion(.failure(.noData))
                 return }
             if error != nil {
-                completionHandler(.failure(.networkError))
+                completion(.failure(.networkError))
                 return
             }
             guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                completionHandler(.failure(.badResponse))
+                (200...299).contains(httpResponse.statusCode) else {
+                    completion(.failure(.badResponse))
+                    return
+            }
+            
+            // MARK: - Response status code
+            if self.decodingDataType == nil {
+                let statusCode = httpResponse.statusCode as! T
+                completion(.success(statusCode))
                 return
             }
-
-            self.decodeService.decodeData(data: data, dataType: decodingDataType, completionHandler: completionHandler)
             
-
+            // MARK: - Decoding data and return object
+            DecodeService().decodeData(data: data, dataType: self.decodingDataType!, completionHandler: completion)
+            
         }.resume()
     }
 }
